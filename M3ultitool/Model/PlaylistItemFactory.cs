a9dad49870a5facec56ed1e4239e281fbafe910c
MillionOstrich <31486600+MillionOstrich@users.noreply.github.com>
2018-04-09 18:06:49 +0100

    Add class for creating playlist items from file paths

---
+++ M3ultitool/Model/PlaylistItemFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3ultitool.Model
+{
+    class PlaylistItemFactory
+    {
+        public PlaylistItem FromFile(string path)
+        {
+            PlaylistItem item = null;
+            if(System.IO.File.Exists(path))
+            {
+                try
+                {
+                    using (var file = TagLib.File.Create(path))
+                    {
+                        item = new PlaylistItem(path, file.Tag);
+                    }
+                }
+                catch
+                {
+                    //In cases of unsupported file types or unreadable files, just don't add them to the playlist.
+                }
+            }
+            return item;
+        }
+
+        public IEnumerable<PlaylistItem> FromPathList(StringCollection paths)
+        {
+            foreach (string path in paths)
+            {
+                if (System.IO.Directory.Exists(path))
+                {
+                    foreach (string file in System.IO.Directory.EnumerateFiles(path, "*.*", System.IO.SearchOption.AllDirectories))
+                    {
+                        var result = FromFile(file);
+                        if (result != null) yield return result; 
+                    }
+                }
+                else
+                {
+                    var result = FromFile(path);
+                    if (result != null) yield return result;
+                }
+            }
+        }
+
+    }
+}
