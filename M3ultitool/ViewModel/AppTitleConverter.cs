422df41684f9a18256d2fd69f0a21bb664a261c2
MillionOstrich <31486600+MillionOstrich@users.noreply.github.com>
2018-04-09 18:08:05 +0100

    Added class for generating title bar text from playlist view model

---
+++ M3ultitool/ViewModel/AppTitleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace M3ultitool.ViewModel
+{
+    public class AppTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is PlaylistViewModel vm)
+            {
+                string path = Path.GetFileName(vm.FullPath ?? PlaylistViewModel.DefaultName);
+                return vm.UnsavedChanges ? path + "*" : path;
+            }
+            return "";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
