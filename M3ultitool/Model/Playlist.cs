6a15429aec1d32a9a88c6665418139d5422a5aed
MillionOstrich <31486600+MillionOstrich@users.noreply.github.com>
2018-04-09 04:48:24 +0100

    Initial version of model

---
+++ M3ultitool/Model/Playlist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3ultitool.Model
+{
+    public class Playlist
+    {
+        public string FullPath { get; set; }
+        public ICollection<PlaylistItem> Items { get; set; }
+
+        public Playlist()
+        {
+            FullPath = null;
+            Items = new List<PlaylistItem>();
+        }
+        public Playlist(string path)
+        {
+            FullPath = path;
+            Items = new List<PlaylistItem>();
+        }
+        public Playlist(string path, ICollection<PlaylistItem> items)
+        {
+            FullPath = path;
+            Items = items ?? throw new ArgumentNullException();
+        }
+
+        public Playlist DeepCopy()
+        {
+            List<PlaylistItem> copy = new List<PlaylistItem>(Items.Count);
+            foreach(var x in Items) { copy.Add(x.Copy()); }
+            return new Playlist(FullPath, copy);
+        }
+    }
+}
---
+++ M3ultitool/Model/PlaylistItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TagLib;
+
+namespace M3ultitool.Model
+{
+    public class PlaylistItem
+    {
+        private string _path;
+        private Tag _tags;
+
+        public string FullPath { get { return _path; } }
+        public Tag Tags { get { return _tags; } }
+
+        public PlaylistItem(string path)
+        {
+            _path = path ?? throw new ArgumentNullException();
+            _tags = null;
+        }
+        public PlaylistItem(string path, Tag tags)
+        {
+            _path = path ?? throw new ArgumentNullException();
+           _tags = tags;
+        }
+
+        public PlaylistItem Copy()
+        {
+            return new PlaylistItem(_path, _tags);
+        }
+    }
+}
